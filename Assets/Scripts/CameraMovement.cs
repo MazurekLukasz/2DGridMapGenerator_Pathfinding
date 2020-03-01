b7583a2dc1481a1079b5f8002384e32c2c991ba2
Ciasteczko <mazurek.lukasz.1995@gmail.com>
2020-03-01 12:26:26 +0100

    Add a Zoom in/out system

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,17 +6,22 @@ public class CameraMovement : MonoBehaviour
 {
     [SerializeField] float CamSpeed = 5;
     public bool Pause { get; set; }
+    private float targetZoom;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetZoom = Camera.main.orthographicSize;
     }   
 
     // Update is called once per frame
     void Update()
     {
-        if(!Pause)
-        Movement();
+        if (!Pause)
+        {
+            Movement();
+            Zoom();
+        }
     }
 
     void Movement()
@@ -30,5 +35,14 @@ public class CameraMovement : MonoBehaviour
     public void ChangePosition(Vector3 pos)
     {
         transform.position = pos;
+    }
+
+    private void Zoom()
+    {
+        float scrollData = Input.GetAxis("Mouse ScrollWheel");
+
+        targetZoom -= scrollData * 3f;
+        targetZoom = Mathf.Clamp(targetZoom,3f,20f);
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, targetZoom, Time.deltaTime* 10f);
     }
 }
